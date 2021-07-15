2a639e72d8872c03b01d30ae8c4295c63fa7fad1
jitangupta <hi@jitangupta.com>
2021-07-15 11:40:04 +0530

    gulp related update

--- gulpfile.js
+++ gulpfile.js
@@ -1,60 +1,91 @@
-// Gulpfile.js
+const { src, dest, watch, series, parallel } = require("gulp");
+// Importing all the Gulp-related packages we want to use
+const sass = require("gulp-sass")(require("sass"));
+const concat = require("gulp-concat");
+const terser = require("gulp-terser");
+const postcss = require("gulp-postcss");
+const autoprefixer = require("autoprefixer");
+const cssnano = require("cssnano");
+const replace = require("gulp-replace");
+const browsersync = require("browser-sync").create();
 
-"use strict";
+// File paths
+const files = {
+  scssPath: "src/scss/**/*.scss",
+  jsPath: "src/js/**/*.js",
+};
 
-var gulp = require("gulp"),
-  eslint = require("gulp-eslint"),
-  // less = require("gulp-less"),
-  // minifyCSS = require("gulp-minify-css"),
-  // path = require("path"),
-  notify = require("gulp-notify"),
-  clean = require("gulp-clean"),
-  rename = require("gulp-rename"),
-  // concat = require("gulp-concat"),
-  uglify = require("gulp-uglify");
+function scssTask() {
+  return src(files.scssPath, { sourcemaps: false })
+    .pipe(sass())
+    .pipe(dest("dist/css", { sourcemaps: "." }));
+}
 
-// var less_src = [
-//   "node_modules/bootstrap/less/variables.less", 
-//   "node_modules/bootstrap/less/mixins/*.less",
-//   "src/less/bootstrap-dialog.less"
-// ];
+function scssMinTask() {
+  return src(files.scssPath, { sourcemaps: false })
+    .pipe(sass())
+    .pipe(postcss([autoprefixer(), cssnano()]))
+    .pipe(concat("bootstrap-dialog.min.css"))
+    .pipe(dest("dist/css", { sourcemaps: "." }));
+}
 
-// gulp.task("less", function() {
-//   gulp.src(less_src)
-//     .pipe(concat("bootstrap-dialog.less"))
-//     .pipe(gulp.dest("dist/less"))
-//     .pipe(less())
-//     .pipe(gulp.dest("dist/css"))
-//     .pipe(gulp.dest("src/css"))
-//     .pipe(rename("bootstrap-dialog.min.css"))
-//     .pipe(minifyCSS())
-//     .pipe(gulp.dest("dist/css"));
-// });
+function jsTask() {
+  return src([files.jsPath]).pipe(dest("dist/js"));
+}
 
-gulp.task("lint", function() {
-  gulp.src(["src/js/bootstrap-dialog.js"])
-    .pipe(eslint())
-    .pipe(eslint.format());
-});
+function jsMinTask() {
+  return src([files.jsPath], { sourcemaps: false })
+    .pipe(concat("bootstrap-dialog.min.js"))
+    .pipe(terser())
+    .pipe(dest("dist/js", { sourcemaps: "." }));
+}
 
-gulp.task("dist", ["clean"], function() {
-  gulp.src(["src/js/bootstrap-dialog.js"])
-    .pipe(gulp.dest("dist/js"))
-    .pipe(rename("bootstrap-dialog.min.js"))
-    .pipe(uglify())
-    .pipe(gulp.dest("dist/js"))
-    .pipe(notify({
-      message: "Build task completed."
-    }));
-});
+// Browsersync to spin up a local server
+function browserSyncServe(cb) {
+  browsersync.init({
+    server: {
+      baseDir: ".",
+    },
+    notify: {
+      styles: {
+        top: "auto",
+        bottom: "0",
+      },
+    },
+  });
+  cb();
+}
+function browserSyncReload(cb) {
+  browsersync.reload();
+  cb();
+}
 
-gulp.task("clean", function() {
-  return gulp.src(["dist/"], {
-    read: false
-  })
-    .pipe(clean());
-});
+function watchTask() {
+  watch(
+    [files.scssPath, files.jsPath],
+    { interval: 1000, usePolling: true }, 
+    series(scssTask, scssMinTask, jsTask, jsMinTask)
+  );
+}
+function bsWatchTask() {
+  watch("index.html", browserSyncReload);
+  watch(
+    [files.scssPath, files.jsPath],
+    { interval: 1000, usePolling: true },
+    series(
+      parallel(scssTask, scssMinTask, jsTask, jsMinTask),
+      browserSyncReload
+    )
+  );
+}
 
-gulp.task("default", ["clean"], function() {
-  gulp.start("dist");
-});
+exports.default = series(
+  parallel(scssTask, scssMinTask, jsTask, jsMinTask),
+  watchTask
+);
+
+exports.bs = series(
+  parallel(scssTask, scssMinTask, jsTask, jsMinTask),
+  browserSyncServe,
+  bsWatchTask
+);
